c4408b6e05418158c2875b2e15e169b80341af10
Anthony D <dunahee@gmail.com>
2019-01-18 17:00:44 -0600

    images resized and display like button and like counter

--- src/components/Gallery/GalleryPhoto.js
+++ src/components/Gallery/GalleryPhoto.js
@@ -18,19 +18,34 @@ class GalleryPhoto extends Component {
     photoSide = () => {
         if(this.state.selected === false){
             return (
-                <img
-                    src={this.props.photoObj.path}
-                    key={this.props.key}
-                    alt={this.props.photoObj.description}
-                    onClick={this.handlePhotoClick}>
-                </img>
+                <div >
+                    <img
+                        src={this.props.photoObj.path}
+                        key={this.props.key}
+                        alt={this.props.photoObj.description}
+                        onClick={this.handlePhotoClick}
+                        className="photo-box"
+                        >
+                    </img>
+                    <br/>
+                    <span>{this.props.photoObj.likes} likes.</span>
+                    <br />
+                    <button>Like!</button>
+                </div>
+                
             )
         } else {
             return (
                 <div>
-                    <p onClick={this.handlePhotoClick}>
-                        {this.props.photoObj.description}
-                    </p>
+                    <div className="photo-box">
+                        <p onClick={this.handlePhotoClick}>
+                            {this.props.photoObj.description}
+                        </p>
+                    </div>
+                    <br />
+                    <span>{this.props.photoObj.likes} likes.</span>
+                    <br />
+                    <button>Like!</button>
                 </div>
             )
         }
