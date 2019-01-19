bbb3d019180b217009eabb62399c031d9592ba52
Anthony D <dunahee@gmail.com>
2019-01-18 19:04:14 -0600

    allow user to delete photo from DB and update DOM

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const galleryItems = require('../modules/gallery.data');
+//const galleryItems = require('../modules/gallery.data');
 const pg = require('pg');
 const Pool = pg.Pool;
 
-const pool = new Pool ({
+const pool = new Pool({
     database: 'react-gallery',
     host: 'localhost',
     port: 5432,
@@ -20,30 +20,41 @@ const pool = new Pool ({
 router.put('/like/:id', (req, res) => {
     pool.query(
         `UPDATE "photos" SET "likes" = "likes"+1 WHERE "id" = $1;`,
-        [req.params.id]).then((result)=> {
+        [req.params.id]).then((result) => {
             res.sendStatus(200);
         }).catch((error) => {
             console.log(`Error in /like UPDATE`, error);
             res.sendStatus(500);
         })
-    console.log(req.params);
-    const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
+    // console.log(req.params);
+    // const galleryId = req.params.id;
+    // for(const galleryItem of galleryItems) {
+    //     if(galleryItem.id == galleryId) {
+    //         galleryItem.likes += 1;
+    //     }
+    // }
     res.sendStatus(200);
 }); // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
-    pool.query( `SELECT * FROM "photos" ORDER BY "year" DESC;`).then((result)=>{
+    pool.query(`SELECT * FROM "photos" ORDER BY "year" DESC;`).then((result) => {
         res.send(result.rows);
-    }).catch((error)=>{
+    }).catch((error) => {
         console.log(`Error during get`, error);
         res.sendStatus(error);
     });
 }); // END GET Route
 
-module.exports = router;
+router.delete('/delete/:id', (req, res) => {
+    pool.query(
+        `DELETE FROM "photos" WHERE "id" = $1;`,
+        [req.params.id]).then((result) => {
+            res.sendStatus(200);
+        }).catch((error) => {
+            console.log(`Error in /like UPDATE`, error);
+            res.sendStatus(500);
+        });
+    });
+
+    module.exports = router;
